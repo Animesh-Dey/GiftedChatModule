b6c6c9094f4b92f01881e07ae1d45ab8d04d8108
Animesh Dey <animesh.dey@weavers-web.com>
2023-09-06 16:44:35 +0530

    firebase setup done

--- src/screens/RegisterScreen.js
+++ src/screens/RegisterScreen.js
@@ -3,19 +3,11 @@ import {View, StyleSheet} from 'react-native';
 import {Input, Button} from 'react-native-elements';
 
 const RegisterScreen = () => {
-  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [avatar, setAvatar] = useState('');
 
   return (
     <View style={styles.container}>
-      <Input
-        placeholder="Enter your name"
-        label="Name"
-        value={name}
-        onChangeText={text => setName(text)}
-      />
       <Input
         placeholder="Enter your email"
         label="Email"
@@ -29,12 +21,6 @@ const RegisterScreen = () => {
         onChangeText={text => setPassword(text)}
         secureTextEntry
       />
-      <Input
-        placeholder="Enter your image url"
-        label="Profile Picture"
-        value={avatar}
-        onChangeText={text => setAvatar(text)}
-      />
       <Button title="Register" style={styles.button} />
     </View>
   );
@@ -44,7 +30,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     padding: 10,
-    marginTop: 100,
+    justifyContent: 'center',
   },
   button: {
     width: 370,
